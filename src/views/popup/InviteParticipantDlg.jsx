ce51aeef8a0336b444fcdef098a46ef00b21fcc5
eedys <eedys@ex-em.com>
2021-10-15 01:13:58 +0900

    오류 수정

--- src/views/popup/InviteParticipantDlg.jsx
+++ src/views/popup/InviteParticipantDlg.jsx
@@ -1,39 +1,9 @@
-// import { useCallback } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import Rnb from "../../components/rnb/Rnb";
 
-// const InviteParticipantDlg = () => {
+const InviteParticipantDlg = () => {
+    return (
+        <>
+        </>
+    )
+}
 
-//     const dispatch = useDispatch();
-
-//     const onChange = useCallback((e) => {
-//         const { value } = e.target;
-//         dispatch(search(value));
-//     }, [dispatch]);
-
-//     const onClose = useCallback(() => {
-//         dispatch(deportParticipantPopupClose());
-//     }, [dispatch]);
-
-
-//     const { deportParticipnatPopupView } = useSelector(state => state.cmmn);
-//     const { searchParticipants } = useSelector(state => state.pariticipants);
-
-//     const mapSearchParticipants = searchParticipants.map((participant) => ({
-//         id: participant.participantId,
-//         name: '',
-//         email: participant.userEmail,
-//         isMark: participant.isOwner,
-//         isMarkText: '마스터',
-//     }));
-
-
-
-//     return (
-//         <Rnb 
-//             // rnbView={}    
-//         />
-//     )
-// }
-
-// export default InviteParticipantDlg;
+export default InviteParticipantDlg;
